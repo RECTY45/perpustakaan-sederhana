6ac33f57c4df71ec5a842236b58b8e308a6e42eb
RECTY45 <rectyexploit@gmail.com>
2024-05-26 23:06:11 +0800

    database

--- perpustakaan.sql
+++ perpustakaan.sql
@@ -14,9 +14,54 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
+
+-- Dumping database structure for perpustakaan
+CREATE DATABASE IF NOT EXISTS `perpustakaan` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `perpustakaan`;
+
+-- Dumping structure for table perpustakaan.anggota
+CREATE TABLE IF NOT EXISTS `anggota` (
+  `id_anggota` int NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) DEFAULT NULL,
+  `alamat` text,
+  `no_telp` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `jenis_kelamin` enum('0','1') DEFAULT NULL,
+  PRIMARY KEY (`id_anggota`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
+
+-- Dumping structure for table perpustakaan.buku
+CREATE TABLE IF NOT EXISTS `buku` (
+  `id_buku` int NOT NULL AUTO_INCREMENT,
+  `judul` varchar(255) DEFAULT NULL,
+  `penulis` varchar(255) DEFAULT NULL,
+  `tahun_terbit` int DEFAULT NULL,
+  `genre` varchar(255) DEFAULT NULL,
+  `gambar` varchar(255) DEFAULT NULL,
+  `isbn` varchar(15) DEFAULT NULL,
+  `stok` int DEFAULT NULL,
+  PRIMARY KEY (`id_buku`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table perpustakaan.peminjaman
+CREATE TABLE IF NOT EXISTS `peminjaman` (
+  `id_peminjaman` int NOT NULL AUTO_INCREMENT,
+  `id_anggota` int DEFAULT NULL,
+  `id_buku` int DEFAULT NULL,
+  `tanggal_peminjaman` date DEFAULT NULL,
+  `tanggal_wajib_dikembalikan` date DEFAULT NULL,
+  `tanggal_kembali` date DEFAULT NULL,
+  `status_peminjaman` enum('Dipinjam','Dikembalikan Tepat Waktu','Terlambat Dikembalikan') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `pinjam_berapa_hari` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `perpanjangan_peminjaman` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `status_denda` enum('Tidak Denda','Kena Denda') DEFAULT NULL,
+  `catatan_perpustakaan` text,
+  PRIMARY KEY (`id_peminjaman`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
